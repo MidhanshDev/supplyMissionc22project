0c0e2ccb11a5f335681b6a5641088348f59bbadc
MidhanshDev <68544626+MidhanshDev@users.noreply.github.com>
2020-07-29 18:28:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,7 +59,7 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:false});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:false});
 	World.add(world, packageBody);
   }
 }
